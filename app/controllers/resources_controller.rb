eb7a6c4516cd573725d3454d489687966876ebfc
Ming Zhang <prongzhang@gmail.com>
2015-05-02 18:37:00 -0400

    added post content to learn page

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,8 +1,24 @@
 class ResourcesController < ApplicationController
   def index
+    @resources = Resource.all
   end
 
   def new
    @resource = Resource.new
   end
+
+  def create
+    Resource.create(permitted_params)
+    redirect_to action: 'index'
+  end
+
+
+  private
+
+  def permitted_params
+    params.require(:resource).permit(:title, :content, :user_id) # permit only certain parameters to be inserted into database
+  end
+
 end
+
+
