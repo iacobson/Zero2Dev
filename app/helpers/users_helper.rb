eb05ba63b3cfc6f403156863d6dc6fab84a6fc40
sandiegodj <davidjanczyn@gmail.com>
2015-05-05 13:31:46 -0700

    add thumbnail to header

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,6 @@ module UsersHelper
   def gravatar_for(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravtar_url, alt: user.email, class: "gravatar")
-
+    image_tag(gravatar_url, alt: user.email, class: "gravatar")
   end
 end
