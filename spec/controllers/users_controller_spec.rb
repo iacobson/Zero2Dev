c880f447b170896801f16074ae912d4ccfc3f8c0
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2015-05-06 19:14:48 +0300

    Add test to users_controller show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    # create a user
+    let(:user) {User.create!(name:"John", email:"john@mail.com", password:"password")}
+    # even if the guests do not create an account, it is still good to have access to the users profiles and to see their projects
+    it "shows a user to non-logged-in guest" do
+      get :show, id: user
+      expect(response).to render_template(:show)
+      # show should pass an instance variable @user to the show template
+      expect(assigns(:user)).to eq(user)
     end
 
-    post :create, project: {name: "Runway", tasks: "Start something:2"}
-      expect(response).to redirect_to(projects_path)
-      expect(assigns(:action).project.name).to eq("Runway")
+
 
   end
 
